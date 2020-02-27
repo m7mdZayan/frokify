5e84322e04b9606d2fb2730a41bcdf7d1949a6b8
Mohamed Zayan <m7mdzayan@gmail.com>
2020-02-27 15:30:07 +0200

    search model is finished

--- src/js/index.js
+++ src/js/index.js
@@ -1,22 +1,6 @@
 // Global app controller
 
-import string from "./models/Search";
-import axios from "axios";
+import Search from "./models/Search";
 
-import { add, multiply, ids } from "./views/searchView";
-//import * as x from "./views/searchView";
-console.log(string);
-
-console.log(add(1, 18));
-console.log(multiply(1, 2));
-
-console.log(ids);
-
-async function getResults(query) {
-  const res = await axios(
-    `https://forkify-api.herokuapp.com/api/search?&q=${query}`
-  );
-  console.log(res);
-}
-
-getResults("pizza");
+const search = new Search("pizza");
+search.getResults();
--- src/js/views/searchView.js
+++
@@ -1,3 +0,0 @@
-export const add = (a, b) => a + b;
-export const multiply = (a, b) => a * b;
-export const ids = [0, 1, 2, 3];
