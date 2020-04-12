5576a8cf74bd9355ea3cb7e1d8e90e8f863989ce
Mohamed Zayan <m7mdzayan@gmail.com>
2020-04-12 17:48:40 +0200

    add shopping-list model

--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import * as searchView from "./views/searchView";
 import * as recipeView from "./views/recipeView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 import Recipe from "./models/Recipe";
+import List from "./models/List";
 
 /** Global app state
  * -Search object
@@ -110,3 +111,5 @@ elements.recipe.addEventListener("click", (e) => {
     recipeView.updateServingsIngredients(state.recipe);
   }
 });
+
+window.l = new List();
